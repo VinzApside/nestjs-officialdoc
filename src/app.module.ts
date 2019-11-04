092ec36d680643f744e47d610fcaa94e4cfce185
Vinz <vincentballut@outlook.fr>
2019-11-04 16:54:58 +0000

    providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -13,17 +13,20 @@ import { Observable, of } from 'rxjs';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { updateExpression } from '@babel/types';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './interface/cat.interface';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
   @Post()
-  create(@Body() createCatDto: CreateCatDto): string {
-    return 'this action adds a new cat';
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
   }
 
   @Get()
-  findAll(@Req() request: Request): Observable<any[]> {
-    return of([]);
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
   @Get('/:id')
@@ -31,13 +34,13 @@ export class CatsController {
     return `This action return a #${params.id} cat`;
   }
 
-  @Put('/:id')
-  update(@Param() id: string, @Body() updateCatDto: UpdateCatDto) {
-    return `This action updates a #{id} cat`;
-  }
+  //   @Put('/:id')
+  //   update(@Param() id: string, @Body() updateCatDto: UpdateCatDto) {
+  //     return `This action updates a #{id} cat`;
+  //   }
 
-  @Delete('/:id')
-  remove(@Param() id: string) {
-    return `This action remove a #${id} cat`;
-  }
+  //   @Delete('/:id')
+  //   remove(@Param() id: string) {
+  //     return `This action remove a #${id} cat`;
+  //   }
 }
